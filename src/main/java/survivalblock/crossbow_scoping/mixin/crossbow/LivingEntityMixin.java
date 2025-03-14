51988fda52cd8a9ad2025caa7fd9faae18a38b13
ThePotatoArchivist <josiahfu@gmail.com>
2025-03-14 00:49:16 -0700

    Fix omnicrossbow conflict

--- src/main/java/survivalblock/crossbow_scoping/mixin/crossbow/LivingEntityMixin.java
+++ src/main/java/survivalblock/crossbow_scoping/mixin/crossbow/LivingEntityMixin.java
@@ -1,17 +1,24 @@
 package survivalblock.crossbow_scoping.mixin.crossbow;
 
 import com.llamalad7.mixinextras.injector.ModifyExpressionValue;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.item.CrossbowItem;
 import net.minecraft.item.ItemStack;
 import net.minecraft.item.SpyglassItem;
+import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import survivalblock.crossbow_scoping.common.CrossbowScoping;
 import survivalblock.crossbow_scoping.common.init.CrossbowScopingDataComponentTypes;
 
 @Mixin(LivingEntity.class)
-public class LivingEntityMixin {
+public abstract class LivingEntityMixin extends Entity {
+
+    public LivingEntityMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
 
     @ModifyExpressionValue(method = {"tickActiveItemStack", "consumeItem"}, at = @At(value = "INVOKE", target = "Lnet/minecraft/entity/LivingEntity;getStackInHand(Lnet/minecraft/util/Hand;)Lnet/minecraft/item/ItemStack;"))
     private ItemStack replaceWithScope(ItemStack original) {
@@ -19,6 +26,9 @@ public class LivingEntityMixin {
             return original;
         }
         if (!CrossbowScoping.isLoaded(original)) {
+            return original;
+        }
+        if (CrossbowScoping.OMNICROSSBOW_LOADED && isSneaking()) {
             return original;
         }
         ItemStack stackInComponents = original.getOrDefault(CrossbowScopingDataComponentTypes.CROSSBOW_SCOPE, ItemStack.EMPTY);
